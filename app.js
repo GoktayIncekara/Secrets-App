705ed373fed2f1f9933d44860566178c5b00d162
Göktay İncekara <goktayincekara@gmail.com>
2023-01-23 22:11:04 +0300

    Add first level authentication (username and password)

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //jshint esversion:6
 const express = require("express");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -8,6 +9,16 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
+
+mongoose.set("strictQuery", false);
+mongoose.connect("mongodb://127.0.0.1:27017/userDB", { useNewUrlParser: true });
+
+const userSchema = {
+  email: String,
+  password: String,
+};
+
+const User = new mongoose.model("User", userSchema);
 
 app.listen(3000, function (req, res) {
   console.log("Server started on port 3000.");
@@ -23,4 +34,32 @@ app.get("/login", function (req, res) {
 
 app.get("/register", function (req, res) {
   res.render("register");
+});
+
+app.post("/register", function (req, res) {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
+  newUser.save(function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("secrets");
+    }
+  });
+});
+
+app.post("/login", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+  User.findOne({ email: username }, function (err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser && foundUser.password === password) {
+        res.render("secrets");
+      }
+    }
+  });
 });
